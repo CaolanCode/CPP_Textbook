5b8cea64ab479f53207bfc8b3716c258bffa3233
CaolanCode <cmacmahon3@gmail.com>
2023-01-11 05:57:59 +0000

    commit input two variables program

---
+++ InputTwoVar.cpp
@@ -0,0 +1,16 @@
+/*
+    input two variables and return sum
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+    int sum = x + y;
+    cout << "Their sum is: " << sum << endl;
+    
+    return 0;
+}
